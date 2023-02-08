3fdc477d21df53cf93a4433b9c9dcd227fd586af
Muhammad Tauseef Afzal <MA3618@live.mdx.ac.uk>
2023-02-08 13:59:51 +0000

    first push with assets

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SolutionComponent } from './solution/solution.component';
+import { FeaturesComponent } from './features/features.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes: Routes =[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    // { path: 'about', component: AboutComponent },
+    // { path: 'dashboard', component: DashboardComponent },
+    // { path: 'pricing',component: PricingComponent },
+    // { path: 'solution', component: SolutionComponent },
+    // { path: 'features', component: FeaturesComponent },
+    // { path: 'companies', component: CompaniesComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SolutionComponent } from './solution/solution.component';
+import { FeaturesComponent } from './features/features.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    AboutComponent,
+    PricingComponent,
+    SolutionComponent,
+    FeaturesComponent,
+    CompaniesComponent,
+    DashboardComponent
+  ],
+  imports: [    
+    MatSlideToggleModule,
+    NgbModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
